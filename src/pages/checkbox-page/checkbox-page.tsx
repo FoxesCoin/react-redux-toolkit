32f07c9c4c1f51175e3571d28b4c879d202a1009
FoxesCoin <denis.zs.developer@gmail.com>
2022-11-22 17:59:44 +0200

    update/checkbox-page

--- src/pages/checkbox-page/checkbox-page.tsx
+++ src/pages/checkbox-page/checkbox-page.tsx
@@ -7,6 +7,7 @@ import { toggleItemSimpleArray } from 'services/array';
 
 import { RPage } from 'typings/react';
 
+import { Theme } from 'styles/theme';
 import { CheckboxPageStyles as Styled } from './checkbox-page.styles';
 
 const CHECKBOX_LIST = ['1', '2', '3'].map((data, key) => ({ data, key }));
@@ -24,57 +25,70 @@ const CheckboxPage: RPage = () => {
       <Styled.FiledList>
         <Styled.Legend>Switch (multiple choose)</Styled.Legend>
         {CHECKBOX_LIST.map(({ data, key }) => (
-          <Switch
-            key={key}
-            value={data}
-            isChecked={checkbox.includes(data)}
-            onCheck={handleCheck}
-          />
+          <label key={key}>
+            <Switch
+              value={data}
+              isChecked={checkbox.includes(data)}
+              onChange={handleCheck}
+            />
+            <Theme.Text>{data}</Theme.Text>
+          </label>
         ))}
       </Styled.FiledList>
       <Styled.FiledList>
         <Styled.Legend>Checkbox</Styled.Legend>
         {CHECKBOX_LIST.map(({ data, key }) => (
-          <Radio
-            key={key}
-            value={data}
-            isChecked={checkbox.includes(data)}
-            onCheck={handleCheck}
-          />
+          <label key={key}>
+            <Radio
+              value={data}
+              isChecked={checkbox.includes(data)}
+              onCheck={handleCheck}
+            />
+            <Theme.Text>{data}</Theme.Text>
+          </label>
         ))}
       </Styled.FiledList>
       <Styled.FiledList>
         <Styled.Legend>Switch (single choose)</Styled.Legend>
         {CHECKBOX_LIST.map(({ data, key }) => (
-          <Switch
-            key={key}
-            value={data}
-            isChecked={radio === data}
-            onCheck={setRadio}
-          />
+          <label key={key}>
+            <Switch
+              key={key}
+              value={data}
+              isChecked={radio === data}
+              onChange={setRadio}
+            />
+            <Theme.Text>{data}</Theme.Text>
+          </label>
         ))}
       </Styled.FiledList>
       <Styled.FiledList>
         <Styled.Legend>Checkbox</Styled.Legend>
         {CHECKBOX_LIST.map(({ data, key }) => (
-          <Radio
-            key={key}
-            value={data}
-            isChecked={radio === data}
-            onCheck={setRadio}
-          />
+          <label key={key}>
+            <Radio
+              key={key}
+              value={data}
+              isChecked={radio === data}
+              onCheck={setRadio}
+            />
+            <Theme.Text>{data}</Theme.Text>
+          </label>
         ))}
       </Styled.FiledList>
       <Styled.FiledList>
         <Styled.Legend>Switch (disabled)</Styled.Legend>
         {CHECKBOX_LIST.map(({ data, key }) => (
-          <Switch
-            key={key}
-            value={data}
-            isChecked={false}
-            onCheck={setRadio}
-            isDisabled
-          />
+          <label key={key}>
+            <Switch
+              key={key}
+              value={data}
+              isChecked={false}
+              onChange={setRadio}
+              isDisabled
+            />
+            <Theme.Text>{data}</Theme.Text>
+          </label>
         ))}
       </Styled.FiledList>
     </Styled.Wrapper>
